f10b01a32d9b253fa19a7ed8c3318c97518a5e3b
Terrence Ryan <terry@terrenceryan.com>
2013-05-21 15:25:09 -0400

    Tweaked some more for the content.

--- js/main.js
+++ js/main.js
@@ -8,20 +8,37 @@ function toggleAllSingles(){
 	});
 }
 
+function closeModal(e){
+	e.preventDefault();
+	document.querySelector('#modal').classList.add("hidden");
+}
+
 function toggleLink(e){
 	e.preventDefault();
 	toggleAllSingles();
 }
+function launchModal(e){
+	e.preventDefault();
+	console.log(e);
+	console.log(e.target.hash);
+	var targetContent = document.querySelector(e.target.hash);
+	console.log(targetContent);
+	document.querySelector("#modal-content").innerHTML = targetContent.innerHTML;
+	document.querySelector('#modal').classList.remove("hidden");
+
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-	console.log("Yo");
 	[].forEach.call( document.querySelectorAll('.single-rotator .app'), function(el) {
 		el.addEventListener('click', function() {
 			toggleAllSingles();
 		}, false);
 	});
+	[].forEach.call( document.querySelectorAll('.modal-toggle'), function(el) {
+		el.addEventListener('click', launchModal, false);
+	});
 	document.querySelector("#list-sub-toggle").addEventListener("click", toggleLink);
+	document.querySelector("#modal-box .close").addEventListener("click", closeModal);
 
 });
-
 
